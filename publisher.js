6eec6a59e16f8df3c44c05e3316514807c7dd7d7
Nibras Ohin <ohin@circleci.com>
2021-05-04 00:57:47 -0400

    Update publisher to take as cmd arg

--- publisher.js
+++ publisher.js
@@ -1,6 +1,6 @@
 const amqp = require("amqplib");
 
-const message = { number: 19 };
+const message = { number: process.argv[2] };
 const connect = async () => {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
@@ -8,7 +8,9 @@ const connect = async () => {
     const result = await channel.assertQueue("jobs");
     channel.sendToQueue("jobs", Buffer.from(JSON.stringify(message)));
     console.log(`job sent successfully: ${message.number}`);
-  } catch (ex) {}
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 connect();
